b85d431e89a32a30a2f4cd68fca001b4009ccc31
Adam <burgessad1997@hotmail.co.uk>
2023-10-03 20:47:25 +0100

    Added Abstract Room Class Reactor/Weapon Rooms, rooms have health and actions list, added Abstract Action class and Attack/Defence actions, actions have a stat and a stat adjustment val

--- Assets/RoomController.cs
+++ Assets/RoomController.cs
@@ -4,12 +4,180 @@ using UnityEngine;
 
 public class RoomController : MonoBehaviour
 {
-    private float health = 100;
+
+
+    public enum RoomType
+    {
+        Weapons,
+        Sheilds,
+        Engines,
+        Reactor
+    }
+
+
+    public abstract class RoomAction { 
+
+        public enum Stat
+		{
+            Attack,
+            Defence
+		}
+
+        public Room roomAffected;
+        protected Stat statAffected;
+        protected float statAdjustment;
+
+        public Stat getStat()
+		{
+            return statAffected;
+		}
+
+        public float getStatAdjustment()
+		{
+            return statAdjustment;
+		}
+
+        public abstract string getInfo();
+		
+
+        public virtual void activate()
+		{
+			switch (statAffected)
+			{
+                case Stat.Defence:
+                    roomAffected.defence += statAdjustment;
+                    break;
+                case Stat.Attack:
+                    roomAffected.takeDamage(statAdjustment);
+                    break;
+                default:
+                    break;
+
+			}   
+            
+		}   
+    
+    }
+
+    public class AttackAction : RoomAction
+	{
+        public AttackAction()
+		{
+            statAffected = RoomAction.Stat.Attack;
+            statAdjustment = 15;
+		}
+		public override string getInfo()
+		{
+            return "Attack action dealing " + statAdjustment.ToString() + " damage";
+		}
+	}
+
+    public class DefenceAction : RoomAction
+    {
+        public DefenceAction()
+        {
+            statAffected = RoomAction.Stat.Defence;
+            statAdjustment = 2;
+        }
+        public override string getInfo()
+        {
+            return "Defence action increasing shield by " + statAdjustment.ToString() + " points";
+        }
+    }
+
+    public abstract class Room
+	{
+        public List<RoomAction> roomActions;
+        public RoomType roomType;
+        private float maxHealth = 100;
+        public  float defence = 10;
+        private float health = 100;
+
+
+        public float getHealth()
+		{
+            return health;
+		}
+        public float getMaxHealth()
+		{
+            return maxHealth;
+		}
+        public float getDefence()
+		{
+            return defence;
+		}
+
+        public void takeDamage(float rawDamage)
+        {
+            float damage = rawDamage - defence;
+            health = health < damage ? 0 : health - damage;
+			if (health==0)
+			{
+                onDestroy();
+			}
+        }
+
+        public virtual void onDestroy()
+		{
+            UnityEngine.Debug.Log("Room Gone");
+		}
+
+        public void ListActions()
+		{
+            foreach(RoomAction ra in roomActions)
+			{
+                UnityEngine.Debug.Log(ra.getInfo());
+            }
+		}
+
+
+    }
+
+    public class WeaponsRoom : Room
+	{
+
+        public WeaponsRoom()
+		{
+            roomType = RoomType.Weapons;
+        }
+
+	}
+
+    public class ReactorRoom : Room
+	{
+
+        public ReactorRoom()
+		{
+            roomType = RoomType.Reactor;
+            roomActions = new List<RoomAction>();
+            roomActions.Add(new AttackAction());
+            roomActions.Add(new DefenceAction());
+        }
+
+		public override void onDestroy()
+		{
+			base.onDestroy();
+            UnityEngine.Debug.Log("Ship gone");
+		}
+
+	}
+
+
+
+
+
+
+
+    
+    private Room room;
     private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
+        room = new ReactorRoom();
+        UnityEngine.Debug.Log(room.roomType);
+        room.ListActions();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
@@ -22,18 +190,17 @@ public class RoomController : MonoBehaviour
     {
         GameManager.Instance.FireLaserAtTarget(Camera.main.ScreenToWorldPoint(Input.mousePosition));
 
-        TakeDamage(33f);
+        room.takeDamage(33f);
+        UpdateHealthBar();
     }
-
-    public void TakeDamage(float damage)
-    {
-        health = health < damage ? 0 : health - damage;
-
+    void UpdateHealthBar()
+	{
         // Increase the red component of the color
-        float redness = 1 - (health/100);
+        float redness = 1 - (room.getHealth() / room.getMaxHealth());
         Color currentColor = spriteRenderer.color;
-        currentColor.r = currentColor.r + ((1-currentColor.r) * redness);
+        currentColor.r = currentColor.r + ((1 - currentColor.r) * redness);
         spriteRenderer.color = currentColor;
-
     }
+
+    
 }
